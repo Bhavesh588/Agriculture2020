9f5bc146d873266045dcb99a9dde3bb89dfbc316
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2020-08-04 17:44:14 +0530

    Updating

--- routers/admin.js
+++ routers/admin.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 let Admin = require('../models/admin.model');
 
-router.get('/' , (req, res) => {
+router.route('/').get((req, res) => {
 	Admin.find()
 	.then(admin => res.json(admin))
 	.catch(err => res.status(400).json('Error: ' + err));
