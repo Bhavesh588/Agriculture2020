05cbad9d07d6eba36a83e278ba2d6731baca008a
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2020-08-04 15:01:00 +0530

    Some Changes in database

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use('/admin', adminRouter);
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    // })
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
 }
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
