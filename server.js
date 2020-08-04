388a92ca8f826911f58f78c1f309260f09c7c7e4
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2020-08-04 15:55:39 +0530

    Changes

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.use('/admin', adminRouter);
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
+    app.get('/admin', (req,res) => {
+        console.log('Sent list of items');
+    })
+
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
